4fc4526e0f6de32b315cf7e4c70e944ef7597bae
Quinton <priceqp@gmail.com>
2019-05-07 22:47:00 +0000

    add mongodb

--- app.js
+++ app.js
@@ -16,7 +16,16 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index");
 
-mongoose.connect("mongodb://localhost:27017/yelp_camp_v12", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://qprice78:Oneman78@cluster0-jwyrx.mongodb.net/yelp_camp?retryWrites=true",{
+    useNewUrlParser: true,
+    useCreateIndex: true
+}).then(() => {
+    console.log("Connected to DB!");
+}).catch(err => {
+    console.log("ERROR:", err.message);
+});
+
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
