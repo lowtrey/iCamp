3ad8fa6b300f2e5b5a294cc98aa3f2ae981f2a5f
Q. Price <priceqp@gmail.com>
2019-05-25 17:11:26 -0500

    Refactor routing syntax

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var express        = require("express"),
     Comment        = require("./models/comment"),
     User           = require("./models/user"),
     seedDB         = require("./seeds");
-    
+
 // Requiring Routes
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
@@ -57,6 +57,6 @@ app.use("/", indexRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
-app.listen(process.env.PORT, process.env.IP, function(){
+app.listen(process.env.PORT || 3000, process.env.IP, function(){
     console.log("The YelpCamp Server Has Started!");
 });
